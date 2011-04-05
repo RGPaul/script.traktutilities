f716921e34dfd6f962cf5a003118bcba9cfb8390
Manromen <rgpaul03@me.com>
2011-04-05 19:53:35 +0200

    added collection update at startup option

--- service.py
+++ service.py
@@ -9,35 +9,31 @@ __settings__ = xbmcaddon.Addon( "script.TraktUtilities" )
 
 Debug("service: " + __settings__.getAddonInfo("id") + " - version: " + __settings__.getAddonInfo("version"))
 
-#autosync_moviecollection = __settings__.getSetting("autosync_moviecollection")
-#autosync_tvshowcollection = __settings__.getSetting("autosync_tvshowcollection")
-autosync_seenmovies = __settings__.getSetting("autosync_seenmovies")
-autosync_seentvshows = __settings__.getSetting("autosync_seentvshows")
-
 # starts update/sync
 def autostart():
     if checkSettings(True):
-        """
-        if autosync_moviecollection:
+        
+        autosync_moviecollection = __settings__.getSetting("autosync_moviecollection")
+        autosync_tvshowcollection = __settings__.getSetting("autosync_tvshowcollection")
+        autosync_seenmovies = __settings__.getSetting("autosync_seenmovies")
+        autosync_seentvshows = __settings__.getSetting("autosync_seentvshows")
+        
+        if autosync_moviecollection == "true":
             notification("Trakt Utilities", "start movie collection update...")
-            #updateMovieCollection(True)
-        if autosync_tvshowcollection:
+            updateMovieCollection(True)
+        if autosync_tvshowcollection == "true":
             notification("Trakt Utilities", "start tvshow collection update...")
-            #updateTVShowCollection(True)
-        """
-        if autosync_seenmovies:
+            updateTVShowCollection(True)
+        if autosync_seenmovies == "true":
             Debug("autostart sync seen movies")
             notification("Trakt Utilities", "start sync seen movies...")
             syncSeenMovies(True)
-        if autosync_seentvshows:
+        if autosync_seentvshows == "true":
             Debug("autostart sync seen tvshows")
             notification("Trakt Utilities", "start sync seen tvshows...")
             syncSeenTVShows(True)
-        """
-        if autosync_moviecollection or autosync_tvshowcollection or autosync_seenmovies or autosync_seentvshows:
+            
+        if autosync_moviecollection == "true" or autosync_tvshowcollection == "true" or autosync_seenmovies == "true" or autosync_seentvshows == "true":
             notification("Trakt Utilities", "update / sync done")
-        """
-        if autosync_seenmovies or autosync_seentvshows:
-            notification("Trakt Utilities", "autosync done")
         
 autostart()
