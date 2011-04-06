d3d0b4b22ca968107f536ee7d00a6240a0c35181
Manromen <rgpaul03@me.com>
2011-04-06 13:57:32 +0200

    added language support for auto sync / update functions

--- service.py
+++ service.py
@@ -6,6 +6,7 @@ import xbmc,xbmcaddon,xbmcgui
 from utilities import *
 
 __settings__ = xbmcaddon.Addon( "script.TraktUtilities" )
+__language__ = __settings__.getLocalizedString
 
 Debug("service: " + __settings__.getAddonInfo("id") + " - version: " + __settings__.getAddonInfo("version"))
 
@@ -19,21 +20,21 @@ def autostart():
         autosync_seentvshows = __settings__.getSetting("autosync_seentvshows")
         
         if autosync_moviecollection == "true":
-            notification("Trakt Utilities", "start movie collection update...")
+            notification("Trakt Utilities", __language__(1180).encode( "utf-8", "ignore" )) # start movie collection update
             updateMovieCollection(True)
         if autosync_tvshowcollection == "true":
-            notification("Trakt Utilities", "start tvshow collection update...")
+            notification("Trakt Utilities", __language__(1181).encode( "utf-8", "ignore" )) # start tvshow collection update
             updateTVShowCollection(True)
         if autosync_seenmovies == "true":
             Debug("autostart sync seen movies")
-            notification("Trakt Utilities", "start sync seen movies...")
+            notification("Trakt Utilities", __language__(1182).encode( "utf-8", "ignore" )) # start sync seen movies
             syncSeenMovies(True)
         if autosync_seentvshows == "true":
             Debug("autostart sync seen tvshows")
-            notification("Trakt Utilities", "start sync seen tvshows...")
+            notification("Trakt Utilities", __language__(1183).encode( "utf-8", "ignore" )) # start sync seen tv shows
             syncSeenTVShows(True)
             
         if autosync_moviecollection == "true" or autosync_tvshowcollection == "true" or autosync_seenmovies == "true" or autosync_seentvshows == "true":
-            notification("Trakt Utilities", "update / sync done")
+            notification("Trakt Utilities", __language__(1184).encode( "utf-8", "ignore" )) # update / sync done
         
 autostart()
