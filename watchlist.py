ea7ed34ace8e5a5014d9a2b8d0832a5be174bd05
Manromen <rgpaul03@me.com>
2011-04-06 14:53:57 +0200

    added file for trending movies / tv shows for watchlist movies / tv shows

---
+++ watchlist.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# @author Ralph-Gordon Paul
+# 
+
+import os
+import xbmc,xbmcaddon,xbmcgui
+import time, socket
+import simplejson as json
+from utilities import *
+
+try:
+    # Python 3.0 +
+    import http.client as httplib
+except ImportError:
+    # Python 2.7 and earlier
+    import httplib
+
+try:
+  # Python 2.6 +
+  from hashlib import sha as sha
+except ImportError:
+  # Python 2.5 and earlier
+  import sha
+  
+try:
+    # Python 2.4
+    import pysqlite2.dbapi2 as sqlite3
+except:
+    # Python 2.6 +
+    import sqlite3 
+
+# read settings
+__settings__ = xbmcaddon.Addon( "script.TraktUtilities" )
+__language__ = __settings__.getLocalizedString
+
+apikey = '0a698a20b222d0b8637298f6920bf03a'
+username = __settings__.getSetting("username")
+pwd = sha.new(__settings__.getSetting("password")).hexdigest()
+debug = __settings__.getSetting( "debug" )
+
+conn = httplib.HTTPConnection('api.trakt.tv')
+headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+
+# list watchlist movies
+def showWatchlistMovies():
+    options = []
+    
+    while True:
+        select = xbmcgui.Dialog().select(__language__(1252).encode( "utf-8", "ignore" ), options) # Watchlist Movies
+        Debug("Select: " + str(select))
+        if select == -1:
+            Debug ("menu quit by user")
+            return
+
+# list watchlist tv shows
+def showWatchlistTVShows():
+    pass
+    
