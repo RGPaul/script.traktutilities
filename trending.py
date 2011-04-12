f667e8fdc755d1463da47ae13fc8a7a684bd6dde
Manromen <rgpaul03@me.com>
2011-04-12 07:21:13 +0200

    added menu for trending movies

--- trending.py
+++ trending.py
@@ -41,38 +41,128 @@ debug = __settings__.getSetting( "debug" )
 conn = httplib.HTTPConnection('api.trakt.tv')
 headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
 
+BACKGROUND = 102
+TITLE = 103
+OVERVIEW = 104
+POSTER = 105
+YEAR = 107
+RUNTIME = 108
+TAGLINE = 109
+MOVIE_LIST = 110
+RATING = 111
+WATCHERS = 112
+
+#get actioncodes from keymap.xml
+ACTION_PREVIOUS_MENU = 10
+ACTION_SELECT_ITEM = 7
+
 def showTrendingMovies():
 
-    options = []
-    movies = []
-    data = getTrendingMoviesFromTrakt()
+    movies = getTrendingMoviesFromTrakt()
     
-    if data == None: # data = None => there was an error
+    if movies == None: # data = None => there was an error
         return # error already displayed in utilities.py
-
-    for movie in data:
-        try:
-            options.append(movie['title'])
-            movies.append(movie)
-        except KeyError:
-            pass # Error ? skip this movie
     
-    if len(options) == 0:
+    if len(movies) == 0:
         xbmcgui.Dialog().ok("Trakt Utilities", "there are no trending movies")
         return
     
-    while True:
-        select = xbmcgui.Dialog().select(__language__(1250).encode( "utf-8", "ignore" ), options) # Trending Movies
-        Debug("Select: " + str(select))
-        if select == -1:
-            Debug ("menu quit by user")
-            return
+    # display trending movie list
+    ui = TrendingMoviesWindow("trending-movies.xml", __settings__.getAddonInfo('path'), "Default")
+    ui.initWindow(movies)
+    ui.doModal()
+    del ui
+
+# @author Ralph-Gordon Paul, Adrian Cowan (othrayte)
+class TrendingMoviesWindow(xbmcgui.WindowXML):
+
+    movies = None
+
+    def initWindow(self, movies):
+        self.movies = movies
         
-        movie_id = getMovieIdFromXBMC(movies[select]['imdb_id'], movies[select]['title'])
-        if movie_id == -1:
-            xbmcgui.Dialog().ok("Trakt Utilities", movies[select]['title'].encode( "utf-8", "ignore" ) + " " + __language__(1162).encode( "utf-8", "ignore" )) # "moviename" not found in your XBMC Library
-        else:
-            playMovieById(movie_id)
+    def onInit(self):
+        from utilities import Debug
+        if self.movies != None:
+            for movie in self.movies:
+                self.getControl(MOVIE_LIST).addItem(xbmcgui.ListItem(movie['title'], '', movie['images']['poster']))
+            self.setFocus(self.getControl(MOVIE_LIST))
+            self.listUpdate()
+
+    def listUpdate(self):
+        from utilities import Debug
+        try:
+            current = self.getControl(MOVIE_LIST).getSelectedPosition()
+        except TypeError:
+            return # ToDo: error output
+        
+        try:
+            self.getControl(BACKGROUND).setImage(self.movies[current]['images']['fanart'])
+        except KeyError:
+            Debug("KeyError for Backround")
+        except TypeError:
+            Debug("TypeError for Backround")
+        try:
+            self.getControl(TITLE).setLabel(self.movies[current]['title'])
+        except KeyError:
+            Debug("KeyError for Title")
+        except TypeError:
+            Debug("TypeError for Title")
+        try:
+            self.getControl(OVERVIEW).setText(self.movies[current]['overview'])
+        except KeyError:
+            Debug("KeyError for Overview")
+        except TypeError:
+            Debug("TypeError for Overview")
+        try:
+            self.getControl(YEAR).setLabel("Year: " + str(self.movies[current]['year']))
+        except KeyError:
+            Debug("KeyError for Year")
+        except TypeError:
+            Debug("TypeError for Year")
+        try:
+            self.getControl(RUNTIME).setLabel("Runtime: " + str(self.movies[current]['runtime']) + " Minutes")
+        except KeyError:
+            Debug("KeyError for Runtime")
+        except TypeError:
+            Debug("TypeError for Runtime")
+        try:
+            self.getControl(TAGLINE).setLabel(self.movies[current]['tagline'])
+        except KeyError:
+            Debug("KeyError for Tagline")
+        except TypeError:
+            Debug("TypeError for Tagline")
+        try:
+            self.getControl(RATING).setLabel("Rating: " + self.movies[current]['certification'])
+        except KeyError:
+            Debug("KeyError for Rating")
+        except TypeError:
+            Debug("TypeError for Rating")
+        try:
+            self.getControl(WATCHERS).setLabel(str(self.movies[current]['watchers']) + " people watching")
+        except KeyError:
+            Debug("KeyError for Watchers")
+        except TypeError:
+            Debug("TypeError for Watchers")
+        
+    def onFocus( self, controlId ):
+    	self.controlId = controlId
+
+    def onAction(self, action):
+        from utilities import Debug
+        
+        if action == ACTION_PREVIOUS_MENU:
+            Debug("Closing TrendingMoviesWindow")
+            self.close()
+        elif action.getId() in (1,2,107):
+            self.listUpdate()
+        elif action.getId() == ACTION_SELECT_ITEM:
+            movie = self.movies[self.getControl(MOVIE_LIST).getSelectedPosition()]
+            movie_id = getMovieIdFromXBMC(movie['imdb_id'], movie['title'])
+            if movie_id == -1: # Error
+                xbmcgui.Dialog().ok("Trakt Utilities", movie['title'].encode( "utf-8", "ignore" ) + " " + __language__(1162).encode( "utf-8", "ignore" )) # "moviename" not found in your XBMC Library
+            else:
+                playMovieById(movie_id)
 
 def showTrendingTVShows():
 
