627faf312d5dee94a44211b082669fc6d72de22e
Adrian Cowan (othrayte) <othrayte@gmail.com>
2011-07-19 17:13:31 +1000

    Added season number to fields returned by getSeasonsFromXBMC(), part of #81

--- utilities.py
+++ utilities.py
@@ -271,7 +271,7 @@ def getTVShowsFromXBMC():
 # get seasons for a given tvshow from XBMC
 def getSeasonsFromXBMC(tvshow):
     Debug("getSeasonsFromXBMC: "+str(tvshow))
-    rpccmd = json.dumps({'jsonrpc': '2.0', 'method': 'VideoLibrary.GetSeasons','params':{'tvshowid': tvshow['tvshowid']}, 'id': 1})
+    rpccmd = json.dumps({'jsonrpc': '2.0', 'method': 'VideoLibrary.GetSeasons','params':{'tvshowid': tvshow['tvshowid'], 'fields': ['season']}, 'id': 1})
     
     result = xbmc.executeJSONRPC(rpccmd)
     result = json.loads(result)
